ebf2e15791beeec2a9f104974c28c57fc499b597
Sitedudev <84919043+Sitedudev@users.noreply.github.com>
2025-08-03 15:11:41 +0200

    Delete index.js

--- index.js
+++
@@ -1,5 +0,0 @@
-const { TahomaPlatform } = require('./platform');
-
-module.exports = (homebridge) => {
-  homebridge.registerPlatform('homebridge-somfy-tahoma-gate', 'TahomaPortail', TahomaPlatform);
-};
